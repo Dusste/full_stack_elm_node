8355d97fdcf578678419652e25a3d962a2de1373
Dusan Stevanovic <dusan.stevanovic@kindredgroup.com>
2022-12-24 15:47:31 +0100

    Testing if prod env variables are working

--- connect-database.js
+++ connect-database.js
@@ -3,20 +3,11 @@ const { Client } = require('cassandra-driver');
 const run = async () => {
     const client = new Client({
         cloud: {
-            secureConnectBundle:
-                process.env.NODE_ENV === 'development'
-                    ? process.env.PATH_TO_DB
-                    : process.env.PATH_TO_DB_PROD,
+            secureConnectBundle: process.env.PATH_TO_DB_PROD,
         },
         credentials: {
-            username:
-                process.env.NODE_ENV === 'development'
-                    ? process.env.DEV_DB_CREDENTIALS_USERNAME
-                    : process.env.DB_CREDENTIALS_USERNAME,
-            password:
-                process.env.NODE_ENV === 'development'
-                    ? process.env.DEV_DB_CREDENTIALS_PASSWORD
-                    : process.env.DB_CREDENTIALS_PASSWORD,
+            username: process.env.DB_CREDENTIALS_USERNAME,
+            password: process.env.DB_CREDENTIALS_PASSWORD,
         },
     });
     try {
